6d6b89dbc5b098e09996f9cb2738a1d441f43c46
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-07-26 19:03:13 +0000

    pointer exercise compiles correctly

--- pointer_exercise.c
+++ pointer_exercise.c
@@ -8,15 +8,21 @@ int main (void)
     int* x;
     int* y;
 
+    // int *a = malloc(size * sizeof *a);
     x = malloc(sizeof(int));
     y = malloc(sizeof(int));
 
     *x = 1;
     *y = 2;
 
-    y = x;
+    x = y;
 
+    printf("value of x = %d, value of y = %d\n", *x, *y);
 
+    free(y);  // remember to free the memory
+    free(x);  // remember to free the memory
+
+    return 0;
 }
 
 
